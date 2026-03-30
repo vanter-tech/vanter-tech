36d2488eecf9c12bbd1b9853951ece1b6d230a72
Fernand-O-band01 <112786231+Fernand-O-band01@users.noreply.github.com>
2026-03-29 23:43:59 -0600

    feat(ui): i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,12 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss(),]
+  },
+  i18n: {
+    locales: ['en', 'es', 'fr', 'jp'],
+    defaultLocale: 'en',
+    routing: {
+      prefixDefaultLocale: false
+    }
   }
 });
---
+++ src/i18n/ui.ts
@@ -0,0 +1,31 @@
+export const languages = {
+  en: "English",
+  es: "Español",
+  fr: "Français",
+  jp: "日本語"
+};
+
+export const defaultLanguage = "en";
+
+export const ui = {
+    en: {
+        mainTitle: "Building high-performance software and digital infrastructure.",
+        diveIntoIt: "Dive into it",
+        getStarted: "Get Started"
+    },
+    es: {
+        mainTitle: "Construyendo software de alto rendimiento y infraestructura digital.",
+        diveIntoIt: "Sumérgete en ello",
+        getStarted: "Comenzar"
+    },
+    fr: {
+        mainTitle: "Construisant un logiciel à haut débit et une infrastructure digitale.",
+        diveIntoIt: "Plongez dedans",
+        getStarted: "Commencer"
+    },
+    jp: {
+        mainTitle: "高性能なソフトウェアとデジタルインフラを構築する。",
+        diveIntoIt: "詳しく見る",
+        getStarted: "始めよう"
+    }
+}as const;
