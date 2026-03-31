d8aa2d579e570baad2cbf24da56fe4b1799bc3d7
Fernand-O-band01 <112786231+Fernand-O-band01@users.noreply.github.com>
2026-03-30 18:26:14 -0600

    feat: splitting i18n file into json files

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,3 +1,8 @@
+import en from "./locales/en.json";
+import es from "./locales/es.json";
+import fr from "./locales/fr.json";
+import jp from "./locales/jp.json";
+
 export const languages = {
   en: "English",
   es: "Español",
@@ -8,46 +13,10 @@ export const languages = {
 export const defaultLanguage = "en";
 
 export const ui = {
-    en: {
-        'nav.about': "ABOUT US",
-        'nav.services': "SERVICES",
-        'nav.contact': "CONTACT US",
-        'hero.mainTitle': "Building high-performance software and digital infrastructure.",
-        'btn.diveIntoIt': "Dive into it",
-        'btn.getStarted': "Get Started",
-        'btn.pricing': "Pricing",
-        'btn.more': "More..."
-    },
-    es: {
-        'nav.about': "SOBRE NOSOTROS",
-        'nav.services': "SERVICIOS",
-        'nav.contact': "CONTACTO",
-        'hero.mainTitle': "Construyendo software de alto rendimiento e infraestructura digital.",
-        'btn.diveIntoIt': "Sumérgete en ello",
-        'btn.getStarted': "Comenzar",
-        'btn.pricing': "Precios",
-        'btn.more': "Más..."
-    },
-    fr: {
-        'nav.about': "À PROPOS DE NOUS",
-        'nav.services': "SERVICES",
-        'nav.contact': "CONTACTEZ-NOUS",
-        'hero.mainTitle': "Construisant un logiciel à haut débit et une infrastructure digitale.",
-        'btn.diveIntoIt': "Plongez dedans",
-        'btn.getStarted': "Commencer",
-        'btn.pricing': "Tarification",
-        'btn.more': "Plus..."
-    },
-    jp: {
-        'nav.about': "私たちについて",
-        'nav.services': "サービス",
-        'nav.contact': "お問い合わせ",
-        'hero.mainTitle': "高性能なソフトウェアとデジタルインフラを構築する。",
-        'btn.diveIntoIt': "詳しく見る",
-        'btn.getStarted': "始めよう",
-        'btn.pricing': "料金",
-        'btn.more': "もっと..."
-    }
+    en,
+    es,
+    fr,
+    jp
 }as const;
 
 export function useTranslation(lang: keyof typeof ui) {
